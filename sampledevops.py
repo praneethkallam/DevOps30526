e9f34bc3a1d6f02a91304fa68032857b452348d4
praneethkallam <43537756+praneethkallam@users.noreply.github.com>
2019-11-20 14:46:56 +0530

    Update sampledevops.py

--- sampledevops.py
+++ sampledevops.py
@@ -1,45 +1,4 @@
-import os
-import sys
+import time 
 
-
-while True: 
-    print("Select operation.")
-    print("1.Add")
-    print("2.Subtract")
-    print("3.Multiply")
-    print("4.Divide")
-
-    def add (x, y):
-        return x + y
-
-    def subtract (x, y):
-        return x - y
-
-    def multiply(x, y):
-        return x * y
-
-    def divide(x ,y):
-        return x / y
-
-
-    choice = input("Enter choice(1/2/3/4): ")
-    if choice > '4.': # (REFER TO TAG 1) : Seeing if this would work ( something to compare to )
-        print ("Invalid Input")
-        answer = input('Run again? (y/n): ')
-        if answer == ('y'):
-            continue
-        if answer == ('n'):
-            sys.exit()
-             
-    num1 = float((input("Enter first number: "))) # Got rid of float() before input UPDATE: you NEED to add float to change it to a decimal number!, earlier without float it would do for example, 12+12 without float would be 1212 but with float it would be 24.0
-    num2 = float((input("Enter second number: "))) # Got rid of float() before input 
-    if choice == "1": # Changed single speech mark to double.
-        print(num1,"+",num2,"=", add(num1,num2))
-    elif choice == "2": # Changed single speech mark to double.
-        print(num1,"-",num2,"=", subtract(num1,num2))
-    elif choice == "3": # Changed single speech mark to double.
-        print(num1,"*",num2,"=", multiply(num1,num2))
-    elif choice == "4": # Changed single speech mark to double.
-        print(num1,"/",num2,"=", divide(num1,num2))
-    else:
-        print("Invalid Input")
+l_time = time.ctime()
+print("my loc time is : ",l_time)
